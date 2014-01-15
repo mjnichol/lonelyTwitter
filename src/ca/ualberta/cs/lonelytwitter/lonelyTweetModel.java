ce280b2340bb3b45bca8d64ea30d0e7d4eac43d8
Mike Nicholson <mjnichol@ualberta.ca>
2014-01-14 17:35:36 -0700

    Added The basic tweet Model class.

---
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.lonelytwitter;
+import java.util.Date;
+
+
+public class lonelyTweetModel {
+	private String text;
+	private Date timestamp;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	
+}
