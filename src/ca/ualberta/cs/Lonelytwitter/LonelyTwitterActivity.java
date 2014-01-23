2dff560a79363f08e47058ce9374294e066935dc
Mike Nicholson <mjnichol@ualberta.ca>
2014-01-23 15:33:37 -0700

    Changed the activity class to notify the adapter and convert to JSON properly

--- src/ca/ualberta/cs/Lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/Lonelytwitter/LonelyTwitterActivity.java
@@ -28,6 +28,8 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayAdapter<String> adapter;
+	private ArrayList<String> tweet_list;
 
 	
 	/** Called when the activity is first created. */
@@ -46,8 +48,15 @@ public class LonelyTwitterActivity extends Activity {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				//finish();
-				onStart();
+				
+				//onStart();
+				tweet_list.add(new Date().toString() + " | " + text + "\n");
+				
+				/*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+						R.layout.list_item, tweets);*/
+				adapter.notifyDataSetChanged();
+				
+				
 			}
 		});
 	}
@@ -57,14 +66,21 @@ public class LonelyTwitterActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onStart();
 		
-		NormalTweetModel[] tweets = loadFromFile();
-		ArrayAdapter<NormalTweetModel> adapter = new ArrayAdapter<NormalTweetModel>(this,
-				R.layout.list_item, tweets);
+		tweet_list = loadFromFile();
+		adapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, tweet_list);
+		
+		/*ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.list_item, tweets);*/
 		oldTweetsList.setAdapter(adapter);
+		adapter.notifyDataSetChanged();
 	}
 
-	private NormalTweetModel[] loadFromFile() {
-		ArrayList<NormalTweetModel> tweets = new ArrayList<NormalTweetModel>();
+	/* We want to turn all of the strings into loenly tweet models */
+	private ArrayList<String> loadFromFile() {
+		
+		//ArrayList<NormalTweetModel> tweets = new ArrayList<NormalTweetModel>();
+		ArrayList<String> tweets = new ArrayList<String>();
 		//TweetListModel tweets = new TweetListModel();
 		Gson gsonTweet = new Gson();
 		try {
@@ -73,21 +89,18 @@ public class LonelyTwitterActivity extends Activity {
 			FileInputStream fis = openFileInput(FILENAME);
 			InputStreamReader isr = new InputStreamReader(fis);
 			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			NormalTweetModel tweeter = new NormalTweetModel();
-			
+		
 			String line = in.readLine();
-			tweets.add(line);
-			//tweeter = gsonTweet.fromJson(isr, NormalTweetModel.class);
 			
-			ArrayList<String> lines = new ArrayList<String>();
+			tweets.add(line);
+			//tweeter = gsonTweet.fromJson(isr, LonelyTweetModel.class);
+			
+		
 			while (line != null) {
-				lines.add(tweeter.);
+				tweets.add(line);
 				line = in.readLine();
 			}
 			
-			
-			// add the tweet to the list using the controller
-			tweets.add(tweeter);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -95,8 +108,10 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		/* try and unpack the JSON */
 		
-		return tweets.toArray(new NormalTweetModel[tweets.size()]);
+		
+		return tweets;
 	}
 	
 	private void saveInFile(String text, Date date) {
